217aa127d2cddb5a793da18641113bb8f52d0b0c
Liam O'Connor <liamoconnor87@gmail.com>
2024-05-16 14:50:44 +0100

    WIP

--- assets/scripts/popup.js
+++ assets/scripts/popup.js
@@ -1,12 +1,12 @@
-// var isChrome = navigator.userAgent.indexOf("Chrome") !== -1;
-// var isFirefox = navigator.userAgent.indexOf("Firefox") !== -1;
-// var thisBrowser;
+var isChrome = navigator.userAgent.indexOf("Chrome") !== -1;
+var isFirefox = navigator.userAgent.indexOf("Firefox") !== -1;
+var thisBrowser;
 
-// if (isChrome) {
-//     thisBrowser = chrome
-// } else if (isFirefox) {
-//     thisBrowser = browser
-// }
+if (isChrome) {
+    thisBrowser = chrome
+} else if (isFirefox) {
+    thisBrowser = browser
+}
 
 var rrCheckbox = document.getElementById("rr-checkbox");
 var toggleStatus = document.getElementById("switch-status");
@@ -20,43 +20,123 @@ if (rrCheckbox.checked) {
 rrCheckbox.addEventListener("change", function () {
     if (rrCheckbox.checked) {
         toggleStatus.innerHTML = `<strong>O</strong>n`;
-        // thisBrowser.storage.local.set({ toggleState: true });
+        thisBrowser.storage.local.set({ toggleState: true });
     } else {
         toggleStatus.innerHTML = `<strong>Of</strong>f`;
-        // thisBrowser.storage.local.set({ toggleState: false });
+        thisBrowser.storage.local.set({ toggleState: false });
     }
 })
 
-// // Store that bad boy
-// thisBrowser.storage.local.get("toggleState").then(({ toggleState }) => {
-//     if (toggleState) {
-//         rrCheckbox.checked = true;
-//     } else {
-//         rrCheckbox.checked = false;
-//         toggleStatus.innerHTML = `<strong>Of</strong>f`;
-//     }
-// });
+// Get toggle state
+thisBrowser.storage.local.get("toggleState").then(({ toggleState }) => {
+    if (toggleState) {
+        rrCheckbox.checked = true;
+    } else {
+        rrCheckbox.checked = false;
+        toggleStatus.innerHTML = `<strong>Of</strong>f`;
+    }
+});
+
+// Toggle state hotkey
+thisBrowser.commands.onCommand.addListener(function(command) {
+    if (command === "toggle-ruler") {
+        thisBrowser.storage.local.get("toggleState").then(({ toggleState }) => {
+            if (toggleState === true) {
+                thisBrowser.storage.local.set({ toggleState: false });
+                rrCheckbox.checked = false;
+                toggleStatus.innerHTML = `<strong>Of</strong>f`;
+            } else {
+                thisBrowser.storage.local.set({ toggleState: true });
+                rrCheckbox.checked = true;
+                toggleStatus.innerHTML = `<strong>O</strong>n`;
+
+            }
+        })
+    }
+})
 
 // Ruler position
 const options = ["Below cursor", "Above cursor", "On cursor"];
 const arrowLeft = document.getElementById("arrow-left");
 const arrowRight = document.getElementById("arrow-right");
 var rulerPosition = document.getElementById("ruler-position");
-let optionIndex = 0;
-rulerPosition.innerHTML = options[optionIndex];
+let optionIndex;
+
+// Get ruler position
+thisBrowser.storage.local.get("ruler").then(({ ruler }) => {
+    if (ruler) {
+        optionIndex = ruler;
+        rulerPosition.innerHTML = options[optionIndex];
+    }
+    else {
+        optionIndex = 0;
+        rulerPosition.innerHTML = options[optionIndex];
+    }
+});
 
 arrowLeft.addEventListener("click", () => {
     if (optionIndex > 0) {
         optionIndex--;
-        console.log(optionIndex);
         rulerPosition.innerHTML = options[optionIndex];
+        thisBrowser.storage.local.set({ ruler: optionIndex });
     }
 })
 
 arrowRight.addEventListener("click", () => {
     if (optionIndex < 2) {
         optionIndex++;
-        console.log(optionIndex);
         rulerPosition.innerHTML = options[optionIndex];
+        thisBrowser.storage.local.set({ ruler: optionIndex });
     }
+})
+
+// Overlay opacity
+const overlayOpacitySettings = document.getElementById("overlay-opacity");
+thisBrowser.storage.local.get("overlayOpacity").then(({ overlayOpacity }) => {
+    if (overlayOpacity) {
+        overlayOpacitySettings.value = overlayOpacity;
+    }
+    else {
+        overlayOpacitySettings.value = 80;
+    }
+});
+
+overlayOpacitySettings.addEventListener("input", () => {
+    thisBrowser.storage.local.set({
+        overlayOpacity: overlayOpacitySettings.value
+    });
+})
+
+// Overlay colour
+const overlayColourSettings = document.getElementById("overlay-colour");
+thisBrowser.storage.local.get("overlayColour").then(({ overlayColour }) => {
+    if (overlayColour) {
+        overlayColourSettings.value = overlayColour;
+    }
+    else {
+        overlayColourSettings.value = "0, 0, 0";
+    }
+})
+
+overlayColourSettings.addEventListener("input", () => {
+    thisBrowser.storage.local.set({
+        overlayColour: overlayColourSettings.value
+    });
+})
+
+// Line Colour
+const lineColourSettings = document.getElementById("line-colour");
+thisBrowser.storage.local.get("lineColour").then(({ lineColour }) => {
+    if (lineColour) {
+        lineColourSettings.value = lineColour;
+    }
+    else {
+        lineColourSettings.value = "0, 0, 0";
+    }
+})
+
+lineColourSettings.addEventListener("input", () => {
+    thisBrowser.storage.local.set({
+        lineColour: lineColourSettings.value
+    });
 })
--- assets/scripts/reading_ruler.js
+++ assets/scripts/reading_ruler.js
@@ -10,77 +10,88 @@ if (isChrome) {
 
 var styleElement = document.createElement('style');
 styleElement.innerHTML = `
-#rr-line {
-    position: fixed;
-    width: 100%;
-    height: 3px;
-    top: 50%;
-    left: 0;
-    transform: translateY(-50%);
-    pointer-events: none;
-    /* to prevent line from blocking mouse events */
-    background: rgb(0, 0, 0);
-    z-index: 9999;
-    opacity: 0;
-}
 #rr-overlay {
     position: fixed;
     width: 100%;
-    height: 100%;
+    height: calc(100% - 53px);
     bottom: 0;
     left: 0;
-    background-color: rgba(0, 0, 0, 0.8);
     z-index: 9999;
     pointer-events: none;
     opacity: 0;
     transition: opacity 0.3s ease;
+    background-color: rgb(0, 0, 0, .8);
+    border-top: 2px solid rgb(0, 0, 0);
 }`;
 
-var lineDiv = document.createElement('div');
+
 var overlayDiv = document.createElement('div');
-lineDiv.id = 'rr-line';
 overlayDiv.id = 'rr-overlay';
-document.body.appendChild(lineDiv);
 document.body.appendChild(overlayDiv);
 document.body.appendChild(styleElement);
 
-document.addEventListener("mousemove", function (event) {
-    var mouseY = event.clientY + 17;
+document.addEventListener("mousemove", async function (event) {
+    // Janky but working for now
+    thisBrowser.storage.local.get("ruler").then(({ ruler }) => {
+        if (ruler == 2) {
+            mouseY = event.clientY;
+        } else if (ruler == 1) {
+            mouseY = event.clientY - 17;
+        } else {
+            mouseY = event.clientY + 17;
+        }
+    });
+
     var windowHeight = window.innerHeight;
-    var line = document.querySelector("#rr-line");
     var overlay = document.querySelector("#rr-overlay");
     var overlayHeight = windowHeight - mouseY; // Calculate the height from the bottom of the window
 
-    line.style.top = mouseY + "px";
+    thisBrowser.storage.local.get("overlayOpacity").then(({ overlayOpacity }) => {
+        if (overlayOpacity) {
+            opacityValue = overlayOpacity / 100;
+        } else {
+            opacityValue = 0.8;
+        }
+    })
+
+    thisBrowser.storage.local.get("overlayColour").then(({ overlayColour }) => {
+        if (overlayColour) {
+            ovColValue = overlayColour;
+        } else {
+            ovColValue = "0, 0, 0";
+        }
+    })
+
+    thisBrowser.storage.local.get("lineColour").then(({ lineColour }) => {
+        if (lineColour) {
+            lineColValue = lineColour;
+        } else {
+            lineColValue = "0, 0, 0";
+        }
+    })
+
     overlay.style.height = overlayHeight + "px";
     overlay.style.bottom = 0; // Position the overlay at the bottom of the window
     overlay.style.opacity = 1; // Make the overlay visible
-    line.style.opacity = 1;
-
-    // Calculate the height of the line
-    var lineHeight = line.offsetHeight;
-    line.style.top = (mouseY + lineHeight) + "px"; // Position the line so that its bottom is aligned with the mouse cursor
+    overlay.style.backgroundColor = `rgba(${ovColValue}, ${opacityValue})`;
+    overlay.style.borderTopColor = `rgb(${lineColValue})`;
 });
 
 document.addEventListener("mouseout", function (event) {
     // Hide the line and the overlay when the mouse leaves the window
     var overlay = document.querySelector("#rr-overlay");
-    var line = document.querySelector("#rr-line");
     overlay.style.opacity = 0; // Hide the overlay when the mouse leaves the window
-    line.style.opacity = 0;
 });
 
 //  Refresh the page when the toggle state changes
 function refreshPage() {
     thisBrowser.storage.local.get("toggleState").then(({ toggleState }) => {
         if (toggleState) {
-            if (lineDiv && overlayDiv) {
-                lineDiv.style.display = "block";
+            if (overlayDiv) {
                 overlayDiv.style.display = "block";
             }
         } else {
-            if (lineDiv && overlayDiv) {
-                lineDiv.style.display = "none";
+            if (overlayDiv) {
                 overlayDiv.style.display = "none";
             }
         }
@@ -90,3 +101,20 @@ function refreshPage() {
 thisBrowser.storage.onChanged.addListener(refreshPage);
 
 refreshPage();
+
+// Not working
+// Toggle state hotkey
+thisBrowser.commands.onCommand.addListener(function (command) {
+    if (command === "toggle-ruler") {
+        thisBrowser.storage.local.get("toggleState").then(({ toggleState }) => {
+            if (toggleState === true) {
+                thisBrowser.storage.local.set({ toggleState: false });
+                overlayDiv.style.display = "none";
+            } else {
+                thisBrowser.storage.local.set({ toggleState: true });
+                overlayDiv.style.display = "block";
+
+            }
+        })
+    }
+})
